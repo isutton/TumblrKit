49a95fc9d54f50dc53186f508b27f3f5a49ae5bd
Igor Sutton <igor@straydev.com>
2010-07-14 10:43:18 +0200

    Added TKTumblr.h

---
+++ TKTumblr.h
@@ -0,0 +1,22 @@
+//
+//
+//  This file is part of TumblrKit
+//
+//  TumblrKit is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with TumblrKit.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  TKTumblr.h by Igor Sutton on 7/14/10.
+//
+
+
+#define Log(format, ...) NSLog(@"%s:%@", __PRETTY_FUNCTION__, [NSString stringWithFormat:format, ## __VA_ARGS__]);
